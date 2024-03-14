2bbb7073ea679ab65b5cf3e81eae9f8e419b19e5
fernandogatto <fernandogatto17@gmail.com>
2024-03-14 18:10:42 -0300

    feat: add search form component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from "../../components/Header";
+import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
@@ -10,6 +11,8 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
+        <SearchForm />
+        
         <TransactionsTable>
           <tbody>
             <tr>
